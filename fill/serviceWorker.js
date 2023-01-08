e26ee48a59bc1e07c80176acc210601eae72b61f
Anton Böhler <antboehler@gmail.com>
2023-01-09 00:01:04 +0100

    fix service worker

--- fill/serviceWorker.js
+++ fill/serviceWorker.js
@@ -1,37 +1,22 @@
 let cacheName = "fill";
 let filesToCache = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/index.css"
+    "/games/fill/",
+    "/games/fill/index.html",
+    "/games/fill/index.js",
+    "/games/fill/index.css"
 ];
 
-self.addEventListener("install", async (e) => {
-    await e.waitUntil(
-        await (await caches.open(cacheName)).addAll(filesToCache)
-    );
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(filesToCache)));
 });
 
-self.addEventListener("fetch", async (e) => {
-    // cache first
-    let response = await caches.match(e.request);
-    if(response) {
-        return response;
-    }
-
-    try{
-        // network second
-        response = await fetch(e.request);
-        
-        await (await caches.open(cacheName)).put(e.request, response.clone());
-
-        return response;
-    }catch{
-        return new Response("Network error happend", {
-            status: 408,
-            headers: {
-                "Content-Type": "text/plain"
-            }
-        });
-    }
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+        return fetch(event.request);
+        }),
+    );
 });
